8459273c770c09e2001d4b514bd4d2e934bc7001
Praneetbandi1 <praneetbandi@usf.edu>
2024-06-28 22:36:36 -0400

    Implement error handling for data fetching

---
+++ script.js
@@ -0,0 +1,30 @@
+//U46368100
+addEventListener("DOMContentLoaded", (event)=>{
+
+let productCount;
+
+async function fetchProductData(count) {
+    document.getElementById("productName").textContent='';
+    document.getElementById("productPicture").src='';
+    document.getElementById("productPicture").textContent='';
+    const apiEndpointURL="https://ww.course-api.com/react-store-products";
+
+    try{
+        await new Promise(resolve=> setTimeout(resolve,500));
+        const response=await fetch(apiEndpointURL);
+
+        const data=await response.json()
+        .then((data)) =>{
+            document.getElementById("productName").textContent=data[ productCount].name;
+            document.getElementById("productPicture").src=data[productCount].image;
+            document.getElementById("productPicture").width="500";
+            document.getElementById("productDescription").textContent=data[productCount].description;
+            document.getElementById("") 
+
+        }
+        
+        
+
+    }
+}
+})
